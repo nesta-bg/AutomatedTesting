ad49751174f57759dfb3624ef312778c6cf9a79f
nenadnesta <nenadnesta@gmail.com>
2019-06-25 20:42:26 +0200

    Simple Math Test.

---
+++ TestWarrior.UnitTests/Fundamentals/MathTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using TestWarrior.Fundamentals;
+
+namespace TestWarrior.UnitTests.Fundamentals
+{
+    [TestFixture] 
+    public class MathTests
+    {
+        [Test]
+        //One execution path
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        //Two execution paths
+        //thing about a Max method as a black box that takes two arguments, dont watch the implementation of Max method
+        //the same is for all test we are writing
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
