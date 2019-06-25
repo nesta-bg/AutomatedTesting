7f14ebd378ad6a9f6b06b6cd9aa0c265cf5aea6c
nenadnesta <nenadnesta@gmail.com>
2019-06-25 15:38:15 +0200

    MSTest - Initial Test.

---
+++ TestWarrior.UnitTests/Fundamentals/ReservationTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestWarrior.Fundamentals;
+
+namespace TestWarrior.UnitTests
+{
+    [TestClass]
+    //NameOfTheClassWeTest+Tests
+    public class ReservationTests
+    {
+        [TestMethod]
+        //NameOfTheMethodWeTest_Scenario_ExpectedBehavior()
+        public void CanBeCancelledBy_AdminCancelling_ReturnTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnTrue()
+        {
+            // Arrange
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnFalse()
+        {
+            // Arrange
+            var reservation = new Reservation { MadeBy = new User() };
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ TestWarrior.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestWarrior.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestWarrior.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74dee652-e2bd-4e2b-9a6b-936b66f40f0e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
