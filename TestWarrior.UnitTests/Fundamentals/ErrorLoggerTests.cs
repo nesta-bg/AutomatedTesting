760ed7189999cfae6942b57f28adbdbcc18e2bb2
nenadnesta <nenadnesta@gmail.com>
2019-06-26 20:57:37 +0200

    Refactoring - SetUp.

--- TestWarrior.UnitTests/Fundamentals/ErrorLoggerTests.cs
+++ TestWarrior.UnitTests/Fundamentals/ErrorLoggerTests.cs
@@ -7,14 +7,20 @@ namespace TestWarrior.UnitTests.Fundamentals
     [TestFixture]
     public class ErrorLoggerTests
     {
+        private ErrorLogger _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
         [Test]
         public void Log_WhenCalled_SetTheLastErrorProperty()
         {
-            var logger = new ErrorLogger();
+            _logger.Log("a");
 
-            logger.Log("a");
-
-            Assert.That(logger.LastError, Is.EqualTo("a"));
+            Assert.That(_logger.LastError, Is.EqualTo("a"));
         }
 
         [Test]
@@ -23,24 +29,20 @@ namespace TestWarrior.UnitTests.Fundamentals
         [TestCase(" ")]
         public void Log_InvalidError_ThrowArgumentNullException(string error)
         {
-            var logger = new ErrorLogger();
-
             //writing assertions by using a delegate
-            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
-            //Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>);
+            Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+            //Assert.That(() => _logger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>);
         }
 
         [Test]
         public void Log_ValidError_RaiseErrorLoggedEvent()
         {
-            var logger = new ErrorLogger();
-
             var id = Guid.Empty;
-            //we are subscribing to the event before acting (logger.Log("a"))
+            //we are subscribing to the event before acting (_logger.Log("a"))
             //lambda expression is the event handler (in args we got some value (Guid.NewGuid()) if the event raise)
-            logger.ErrorLogged += (sender, args) => { id = args; };
+            _logger.ErrorLogged += (sender, args) => { id = args; };
 
-            logger.Log("a");
+            _logger.Log("a");
 
             Assert.That(id, Is.Not.EqualTo(Guid.Empty));
         }
