685686114af7084d7330ba9d9481c0ea7f43db3f
nenadnesta <nenadnesta@gmail.com>
2019-06-26 16:30:50 +0200

    Testing the Return Type of Methods.

---
+++ TestWarrior.UnitTests/Fundamentals/CustomerControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TestWarrior.Fundamentals;
+
+namespace TestWarrior.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            // NotFound object
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // ActionResult object or one of its derivatives (NotFound object, Ok object)
+            //Assert.That(result, Is.InstanceOf<ActionResult>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
