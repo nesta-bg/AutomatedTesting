4f9f94f04cc20c985d05a923334c9015b33c2a61
nenadnesta <nenadnesta@gmail.com>
2019-06-26 15:13:23 +0200

    Testing Strings.

---
+++ TestWarrior.UnitTests/Fundamentals/HtmlFormatterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TestWarrior.Fundamentals;
+
+namespace TestWarrior.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElemenet()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            // Specific Assertion
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //More General Assertions (a little better, especially if method returns an error message).
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc").IgnoreCase);
+        }
+    }
+}
---
+++ TestWarrior/Fundamentals/HtmlFormatter.cs
@@ -0,0 +1,10 @@
+﻿namespace TestWarrior.Fundamentals
+{
+    public class HtmlFormatter
+    {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
+    }
+}
