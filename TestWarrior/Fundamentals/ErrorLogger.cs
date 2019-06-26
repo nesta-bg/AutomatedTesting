eba6174dd0e4598f6e7fa283b5c264e4321f739f
nenadnesta <nenadnesta@gmail.com>
2019-06-26 18:38:32 +0200

    Testing Void Methods.

---
+++ TestWarrior/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestWarrior.Fundamentals
+{
+    public class ErrorLogger
+    {
+        public string LastError { get; set; }
+
+        public event EventHandler<Guid> ErrorLogged;
+
+        //void functions are by definition command functions
+        public void Log(string error)
+        {
+            if (String.IsNullOrWhiteSpace(error))
+                throw new ArgumentNullException();
+
+            LastError = error;
+
+            // Write an error to the log
+            // ...
+
+            ErrorLogged?.Invoke(this, Guid.NewGuid());
+        }
+    }
+}
