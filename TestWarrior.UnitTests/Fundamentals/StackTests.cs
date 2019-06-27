bb7ae4861d8ee9e05bde29e9ef4b2009160eaacc
nenadnesta <nenadnesta@gmail.com>
2019-06-27 19:37:10 +0200

    Stack - Testing Methods that Return Int/String/Throw Exceptions.

---
+++ TestWarrior.UnitTests/Fundamentals/StackTests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using TestWarrior.Fundamentals;
+
+namespace TestWarrior.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Stack<string> _stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new Stack<string>();
+        }
+
+        [Test]
+        public void Push_ArgIsNull_ThrowArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArg_AddTheObjectToTheStack()
+        {
+            _stack.Push("a");
+
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_ReturnObjectOnTheTop()
+        {
+            // Arange
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            // Act
+            var result = _stack.Pop();
+
+            // Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_RemoveObjectOnTheTop()
+        {
+            // Arange
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            // Act
+            _stack.Pop();
+
+            // Assert
+            Assert.That(_stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_ReturnObjectOnTopOfTheStack()
+        {
+            // Arange
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            // Act
+            var result = _stack.Peek();
+
+            // Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_DontRemoveTheObjectOnTopOfTheStack()
+        {
+            // Arange
+            _stack.Push("a");
+            _stack.Push("b");
+            _stack.Push("c");
+
+            // Act
+            _stack.Peek();
+
+            // Assert
+            Assert.That(_stack.Count, Is.EqualTo(3));
+        }
+    }
+}
