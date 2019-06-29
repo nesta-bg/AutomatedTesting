6fd5ce97ebcc99da9b0dd31af5dea3093b5fd8ad
nenadnesta <nenadnesta@gmail.com>
2019-06-29 15:06:23 +0200

    Creating Mock Objects Using Moq Framework.

--- TestWarrior.UnitTests/Mocking/VideoServiceTests.cs
+++ TestWarrior.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,17 +1,29 @@
 ﻿using NUnit.Framework;
 using TestWarrior.Mocking;
+using Moq;
 
 namespace TestWarrior.UnitTests.Mocking
 {
     [TestFixture]
     public class VideoServiceTests
     {
+        private Mock<IFileReader> _fileReader;
+        private VideoService _videoService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new FakeFileReader());
-            
-            var result = service.ReadVideoTitle();
+            //https://github.com/Moq/moq4/wiki/Quickstart moq documentation
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+   
+            var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
