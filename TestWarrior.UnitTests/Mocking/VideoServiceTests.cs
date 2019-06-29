4286d28f5d778ce4bc453eca6eab31566787697f
nenadnesta <nenadnesta@gmail.com>
2019-06-29 12:48:59 +0200

    Dependency Injection via Constructor Parameters.

--- TestWarrior.UnitTests/Mocking/VideoServiceTests.cs
+++ TestWarrior.UnitTests/Mocking/VideoServiceTests.cs
@@ -9,9 +9,8 @@ namespace TestWarrior.UnitTests.Mocking
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService();
-            service.FileReader = new FakeFileReader();
-
+            var service = new VideoService(new FakeFileReader());
+            
             var result = service.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
--- TestWarrior/Mocking/VideoService.cs
+++ TestWarrior/Mocking/VideoService.cs
@@ -8,18 +8,17 @@ namespace TestWarrior.Mocking
 {
     public class VideoService
     {
-        //some di frameworks cannot inject dependencies via properties
+        private IFileReader _fileReader;
 
-        public IFileReader FileReader { get; set; }
-
-        public VideoService()
+        //Poor man's Dependency Injection
+        public VideoService(IFileReader fileReader = null)
         {
-            FileReader = new FileReader();
+            _fileReader = fileReader ?? new FileReader();
         }
 
         public string ReadVideoTitle()
         {
-            var str = FileReader.Read("video.txt");
+            var str = _fileReader.Read("video.txt");
             var video = JsonConvert.DeserializeObject<Video>(str);
             if (video == null)
                 return "Error parsing the video.";
