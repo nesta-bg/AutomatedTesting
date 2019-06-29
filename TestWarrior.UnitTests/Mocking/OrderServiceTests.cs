aca49eb8e582bf0a3f241e32b31ec4ea030a3969
nenadnesta <nenadnesta@gmail.com>
2019-06-29 16:37:15 +0200

    Testing the Interaction Between Two Objects.

---
+++ TestWarrior.UnitTests/Mocking/OrderServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestWarrior.Mocking;
+
+namespace TestWarrior.UnitTests.Mocking
+{
+    [TestFixture]
+    class OrderServiceTests
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
