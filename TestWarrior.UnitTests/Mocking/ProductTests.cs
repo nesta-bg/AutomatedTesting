53c352ac549c93db4c5f29682244431d92ed0b5a
nenadnesta <nenadnesta@gmail.com>
2019-07-01 12:03:15 +0200

    Product Class - Not Mock Abuse Test.

---
+++ TestWarrior.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestWarrior.Mocking;
+
+namespace TestWarrior.UnitTests.Mocking
+{
+    [TestFixture]
+    class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
