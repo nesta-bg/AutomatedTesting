fc54c5e6d6242c7af8a3dd36991429f86075218b
nenadnesta <nenadnesta@gmail.com>
2019-07-01 22:41:29 +0200

    EmployeeController - Refactoring and Testing.

---
+++ TestWarrior.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestWarrior.Mocking;
+
+namespace TestWarrior.UnitTests.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            controller.DeleteEmployee(1);
+
+            storage.Verify(s => s.DeleteEmployee(1));
+        }
+
+        [Test]
+        public void DeleteEmployeee_WhenCalled_ReturnRedirectResult()
+        {
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            var result = controller.DeleteEmployee(1);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+            //Assert.That(result, Is.InstanceOf<ActionResult>());
+        }
+    }
+}
